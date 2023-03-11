50fbe1c7b8e6fa996ba238cb37755e28fbab271e
gaon12 <34927797+gaon12@users.noreply.github.com>
2023-03-11 21:42:12 +0900

    한국어/영어 일부 지원

---
+++ js/languages.js
@@ -0,0 +1,72 @@
+let lang = navigator.language;
+
+if(lang == "ko-KR") {
+    //Menu
+    var menu_title = "우이하루 프로젝트";
+    var menu_overview = "개요";
+    var menu_statistics = "통계/정보";
+    var menu_infos = "정보";
+    var menu_project_info = "우이하루 프로젝트에 대한 정보";
+    var menu_downloadable = "다운로드 가능한 요소";
+    var menu_timeline = "타임라인 / 로드맵";
+    var menu_intro = "소개";
+    var menu_github = "깃허브";
+    var menu_license = "라이선스";
+    var menu_notice = "공지사항";
+
+    //Downloadable
+    var downloadable_title = "다운로드 가능한 목록";
+
+    //Timeline
+    var timeline_title = "타임라인 / 로드맵";
+    var timeline_subtitle = "우이하루가 프로젝트의 타임라인과 로드맵을 알려드릴게요!";
+    var timeline_slogun = "우이하루가 지나온 발자취, 앞으로 나아갈 로드맵";
+    var timeline_0001_title = "우이하루 프로젝트 시작";
+    var timeline_0001_content = "우이하루 프로젝트가 시작했어요! 많이 부족하지만, 열심히 할게요!";
+    var timeline_0002_title = "우이하루 서비스 시작!";
+    var timeline_0002_content = "우이하루 서비스를 시작했어요!";
+    var timeline_0003_title = "우이하루 도메인 독립";
+    var timeline_0003_content = "우이하루 서비스 도메인(uiharu.dev)이 독립했어요! 물론 'uiharu.gaon.xyz'로도 접속할 수 있어요!";
+    var timeline_0004_title = "우이하루 1주년!(예정)";
+    var timeline_0004_content = "1주년까지 갈 수 있도록 여러분의 도움과 관심이 필요해요!";
+
+    //Footer
+    var footer_first = "우이하루 프로젝트는 MIT 라이선스에요!";
+    var footer_secont = "깃허브 저장소";
+    var footer_third = "를 확인하세요!";
+}
+else {
+    //Menu
+    var menu_title = "Uiharu Project";
+    var menu_overview = "Overview";
+    var menu_statistics = "Statistics/Info";
+    var menu_infos = "Info";
+    var menu_project_info = "Information about the Uiharu project";
+    var menu_downloadable = "Downloadable elements";
+    var menu_timeline = "Timeline / Roadmap";
+    var menu_intro = "Introduction";
+    var menu_github = "Github";
+    var menu_license = "License";
+    var menu_notice = "Notice";
+
+    //Downloadable
+    var downloadable_title = "List of Downloadable";
+
+    //Timeline
+    var timeline_title = "Timeline / Roadmap";
+    var timeline_subtitle = "Uiharu will tell you the timeline and roadmap of the project!";
+    var timeline_slogun = "Uiharu's past footsteps, roadmap for the future";
+    var timeline_0001_title = "Start of the Uiharu project";
+    var timeline_0001_content = "The Uiharu Project has started! We're lacking a lot, but we'll do our best!";
+    var timeline_0002_title = "Uiharu service started!";
+    var timeline_0002_content = "Uiharu service started!";
+    var timeline_0003_title = "Uiharu Domain Independence";
+    var timeline_0003_content = "The Uiharu service domain (uiharu.dev) has become independent! Of course, you can also access it as 'uiharu.gaon.xyz'!";
+    var timeline_0004_title = "Uiharu's 1st Anniversary!";
+    var timeline_0004_content = "We need your help and interest to make it to the 1st anniversary!";
+
+    //Footer
+    var footer_first = "The Uiharu project is MIT licensed!";
+    var footer_second = "Check out the GitHub repo"
+    var footer_third = " for more details!";
+}
